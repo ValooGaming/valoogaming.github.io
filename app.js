eb058373dd5f12c25ab277c15d50dcef63bac993
ValooGaming <lesrageursdu13@gmail.com>
2020-09-02 19:18:25 +0200

    Ajout de la navbar

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let youtubeButton = document.querySelector(".social.youtube");
+youtubeButton.addEventListener("click", () => {
+    open("https://youtube.com/valoogaming")
+})
 
+let discordButton = document.querySelector(".social.discord");
+discordButton.addEventListener("click", () => {
+    open("https://discord.bio/p/valoo")
+})
